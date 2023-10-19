cb03bbecc6c2ed409dec1f9b8aac4625e2c242f1
sayedadinan <147618795+sayedadinan@users.noreply.github.com>
2023-10-19 21:40:45 +0530

    Add files via upload

---
+++ artstorm.js
@@ -0,0 +1,11 @@
+
+let toggleLogo=document.querySelector(' .togglelogo')
+let menu=document.querySelector(' .menu')
+
+toggleLogo.addEventListener('click',function(){
+        menu.classList.toggle('is-active')
+    
+})
+// toggleLogo.addEventListener('mouseout',function(){
+//     alert("dddd")
+// })
